bea4bd94d505716e018b03e02b4085d1f69055c6
Cole Williamson <williamsoncole@gmail.com>
2021-02-02 21:35:03 -0600

    tried moving endpoint out of routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 
@@ -17,7 +18,37 @@ app.use(bodyParser.urlencoded({
   extended: true
 }))
 
-app.use('/cameras', cameras)
+// app.use('/cameras', cameras)
+
+
+
+mongoose.connect(process.env.MONGOURI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+// router.get('/', async(req, res) => {
+//   const cameras = await pool.query(`
+//   SELECT * FROM cameras
+//   `)
+//   res.send(cameras.rows)
+// })
+
+const cameraSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+  photourl: String
+});
+
+const Camera = mongoose.model('camera', cameraSchema, 'cameras');
+
+app.get('/cameras', async(req, res) => {
+  Camera.find(function (err, cameras) {
+    if (err) return console.error(err);
+    res.send(cameras);
+  })
+})
+
+module.exports = router
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
